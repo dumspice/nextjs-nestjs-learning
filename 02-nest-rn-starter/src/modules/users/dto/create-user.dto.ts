23fc1066f6adf2db892a78a0ddb062d6cb3013f0
dumspice <dzunghoang04@gmail.com>
2026-01-06 17:55:35 +0700

    add and find user

--- 02-nest-rn-starter/src/modules/users/dto/create-user.dto.ts
+++ 02-nest-rn-starter/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsEmpty, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Tên không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  @IsEmail({}, { message: 'Email không hợp lệ' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
+  password: string;
+
+  phone: string;
+
+  address: string;
+
+  image: string;
+}
