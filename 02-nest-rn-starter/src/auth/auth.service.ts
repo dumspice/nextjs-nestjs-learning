fd0233252a8231b5174da33a578bb83954132f68
dumspice <dzunghoang04@gmail.com>
2026-01-07 01:58:10 +0700

    JWT

--- 02-nest-rn-starter/src/auth/auth.service.ts
+++ 02-nest-rn-starter/src/auth/auth.service.ts
@@ -10,19 +10,21 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
     const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!isValidPassword) {
-      throw new UnauthorizedException('Sai tài khoản hoặc mật khẩu');
+
+    if (!user || !isValidPassword) {
+      return null;
     }
 
-    const payload = { sub: user._id, username: user.email };
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
     return {
-      // 💡 Here the JWT secret key that's used for signing the payload
-      // is the key that was passsed in the JwtModule
-      access_token: await this.jwtService.signAsync(payload),
-      payload,
+      access_token: this.jwtService.sign(payload),
     };
   }
 }
